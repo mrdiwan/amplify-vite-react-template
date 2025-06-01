4360a7be9ef38f398535aea9c30c141445ab86fb
mrd <micdiw01@aol.com>
2025-06-01 16:00:06 -0400

    Updated text - stock ticker

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
   return (
     <main>
       <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <button onClick={createTodo}>+ new</button>
+      <button onClick={createTodo}>Enter Stock Ticker</button>
       <ul>
         {todos.map((todo) => (
           <li
